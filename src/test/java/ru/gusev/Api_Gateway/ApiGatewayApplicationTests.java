568e4c5cbaf69b719449be9b4954ec93bda05ada
KrisHite <alexgusevmail@gmail.com>
2025-06-06 22:14:14 +0500

    Chtobi ne potryat

---
+++ src/test/java/ru/gusev/Api_Gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gusev.Api_Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
